63fce2345e4199d8ce52a8838523b6d966c9ada5
datastory.cz <honza@datastory.cz>
2018-02-15 15:02:18 +0100

    beta

--- game.js
+++ game.js
@@ -5,40 +5,64 @@ Object.keys(latky).forEach(function(l) {
 })
 menu += '</div>'
 $('#hra').append(menu)
+$('#hra').append('<div id="hvezdy">'
+    + '<div class="persona Armstrong"><img alt="armstrong" src="img/armstrong.jpg" class="ico"><br>Armstrong</div>'
+    + '<div class="persona "><img alt="chambers" src="img/chambers.jpg" class="ico"><br>Chambers</div>'
+    + '</div>')
 $('#hra').append('<div id="sport"><img class="sportimg" src="img/clovek.jpg"></div>')
 $('#details').html('<p class="servisni">Vyber si doping</p>')
 
-function kill() {
-    $('#sport').children().attr('src', 'img/smrt.jpg')
-    $('#details').html('<p class="servisni">Ufetoval jsi se :(</p>')
-    
-
-};
+var list_omit = ['sporty', 'Aplikace', 'zdroje', 'img', 'img_sporty', 'Skupina']
 
 $('.fet').on('click', function(e) {
     $('.servisni').remove();
     var fet = $(this).attr("alt");
-
-    console.log()
-
     $('.sel').removeClass('sel')
     $(this).addClass('sel')
-
-
-    var detLength = $('#details').children().length;
     
     var detail = '<div class="detail" alt="' + fet + '"><b>' + fet + '</b><br>'
 
     Object.keys(latky[fet]).forEach(function(key) {
-        detail += key + ': ' + latky[fet][key] + '<br>'
+        if (list_omit.indexOf(key) == -1) {
+            detail += key + ': ' +  latky[fet][key] + '<br>'
+        }
     })
 
     var imgs = ''
     latky[fet].img_sporty.split(', ').forEach(function(key) {
-        imgs += '<img class="sportimg" src="img/' + key + '.jpg">'
+        imgs += '<img class="sportimg" src="img/' + key + '.gif">'
     });
 
     detail += '</div>'
     $('#details').html(detail)
     $('#sport').html(imgs)
 })
+
+var armstrong = ['Erytropoetin', 'Lidský růstový hormon', 'Testosteron', 'Kortizol', 'Krevní transfúze'];
+var chambers = ['Tetrahydrogestrinon', 'Testosteron', 'Erytropoetin', 'HGH', 'Inzulin', 'Modafinil', 'Lidský růstový hormon'];
+
+function unwind(arr) {
+    var out = '';
+    arr.forEach(function(e) {
+        out += e + '<br>'
+        $('div[alt="' + e + '"]').addClass('sel')
+    })
+
+    return out;
+};
+
+$('.persona').on('click', function(e) {
+    $('.servisni').remove();
+    $('.sel').removeClass('sel')
+    $(this).addClass('sel')
+    var per = e.target.alt;
+
+    if (per == 'armstrong') {
+        $('#sport').html('<img class="sportimg" src="img/cyklo.gif">')
+        $('#details').html('<b>' + unwind(armstrong) + '</b>')
+    } else {
+        $('#sport').html('<img class="sportimg" src="img/sprint.gif">')
+        $('#details').html('<b>' + unwind(chambers) + '</b>')
+    }
+
+});
